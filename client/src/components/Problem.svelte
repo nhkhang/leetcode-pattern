90c9acd4b87919735bc9427c064fe2e0ae83db6e
nhkhang <khangnguyenhoangcse@gmail.com>
2024-08-02 21:14:12 +0700

    Add home page

---
+++ client/src/components/Problem.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Card } from "flowbite-svelte";
+  import { Rating, AdvancedRating, ScoreRating } from "flowbite-svelte";
+  import { ArrowUpRightFromSquareOutline } from "flowbite-svelte-icons";
+
+  export let title = "";
+  export let difficulty = "";
+  export let description = "";
+  export let likes = 0;
+  export let link = "#";
+
+</script>
+
+<Card>
+  <h5
+    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+  >
+    {title}
+  </h5>
+  <p class="mb-2"><strong>Difficulty:</strong>{difficulty}</p>
+  <p class="text-gray-700 mb-4">{description}</p>
+  <p><strong>Likes:</strong>{likes}</p>
+  <a href="/" class="inline-flex items-center text-primary-600 hover:underline">
+    View on LeetCode
+    <ArrowUpRightFromSquareOutline class="w-4 h-4 ms-2.5" />
+  </a>
+</Card>
+
+
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,82 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import {
+    Navbar,
+    NavBrand,
+    NavHamburger,
+    NavUl,
+    NavLi,
+    Button,
+    Card,
+    Footer,
+  } from "flowbite-svelte";
+  import { FireSolid } from "flowbite-svelte-icons";
+  import Problem from "../components/Problem.svelte";
+</script>
+
+<Navbar let:hidden let:toggle color="primary">
+  <NavBrand href="/">
+    <!-- <img src="/static/logo.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-red-800"
+      >LeetCode Pattern Detector</span
+    >
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/patterns">Patterns</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
+
+<main class="flex-grow">
+  <section class="hero bg-cover bg-center">
+    <div class="container mx-auto text-center py-20">
+      <h1 class="text-5xl font-bold mb-4">
+        Welcome to LeetCode Pattern Detector
+      </h1>
+      <p class="text-xl mb-8">
+        Solve LeetCode challenges more efficiently by detecting patterns.
+      </p>
+      <!-- <Button color="primary" href="/patterns">Get Started</Button> -->
+    </div>
+  </section>
+
+  <section class="flex flex-grow justify-center">
+    <Problem
+      title="Two Sum"
+      difficulty="Easy"
+      description="Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target."
+      likes={11779}
+      link="https://leetcode.com/problems/two-sum/"
+    />
+  </section>
+</main>
+
+<!-- <Footer class="footer p-4 bg-gray-800 text-white text-center">
+  <p>&copy; 2024. Developed by nhkhang</p>
+</Footer> -->
+
+<style>
+  html,
+  body {
+    height: 100%;
+    margin: 0;
+  }
+  body {
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+  }
+  main {
+    flex-grow: 1;
+  }
+
+  footer {
+    clear: both;
+    position: relative;
+    height: 200px;
+    margin-top: -200px;
+  }
+</style>
